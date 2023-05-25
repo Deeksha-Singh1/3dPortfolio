f2f215595033684051d558ac823b24608acafef3
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-05-25 20:02:21 +0530

    helper functions

---
+++ server/users.js
@@ -0,0 +1,34 @@
+const users = [];
+
+const addUser= ({id,name,room}) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  //if user exists with same name and room then throw error
+  const existingUser = users.find(user.name === name && user.room === room);
+  if(existingUser){
+    return {error: 'Username is taken'};
+  }
+
+  //creating new user
+  const user = {id,name,room};
+
+  users.push(user);
+
+  return {user};
+}
+
+const removeUser = (id) => {
+  const index = users.findIndex((user)=> user.id ===id);
+  if(index!== -1){
+
+    //this will remove the user from the users array
+    return users.splice(index,1)[0];
+  }
+}
+
+const getUser = (id) => users.find((user)=> user.id ===id);
+
+const getUsersInRoom = (room) => users.filter((user)=> user.room ===room) ;
+
+module.exports = {addUser, removeUser, getUser, getUsersInRoom };
